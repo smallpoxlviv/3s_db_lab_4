ec7b8ff716988dd21a8fadfcf83eab737e5b6af8
smallpoxlviv <57070960+smallpoxlviv@users.noreply.github.com>
2020-11-26 21:30:37 +0200

    Update ConnectionManager.java

--- vetclinic/src/main/java/ua/iot/lviv/persistant/ConnectionManager.java
+++ vetclinic/src/main/java/ua/iot/lviv/persistant/ConnectionManager.java
@@ -3,9 +3,9 @@ package ua.iot.lviv.persistant;
 import java.sql.*;
 
 public class ConnectionManager {
-    private static final String URL = "jdbc:mysql://localhost:3306/s3_db_lab_4?serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true";
-    private static final String USER = "student";
-    private static final String PASSWORD = "1234";
+    private static final String URL = "jdbc:mysql://localhost:3306/db_name?serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true";
+    private static final String USER = "user";
+    private static final String PASSWORD = "pass";
 
     private static Connection connection = null;
 
@@ -24,30 +24,6 @@ public class ConnectionManager {
         }
         return connection;
     }
-//
-//    public static void closeStatement(Statement statement) {
-//        if (statement != null) {
-//            try {
-//                statement.close();
-//            } catch (SQLException e) {
-//                System.out.println("SQLException: " + e.getMessage());
-//                System.out.println("SQLState: " + e.getSQLState());
-//                System.out.println("VendorError: " + e.getErrorCode());
-//            }
-//        }
-//    }
-//
-//    public static void closeResultSet(ResultSet resultSet) {
-//        if (resultSet != null) {
-//            try {
-//                resultSet.close();
-//            } catch (SQLException e) {
-//                System.out.println("SQLException: " + e.getMessage());
-//                System.out.println("SQLState: " + e.getSQLState());
-//                System.out.println("VendorError: " + e.getErrorCode());
-//            }
-//        }
-//    }
 
     public static void closeConnection() {
         if (connection != null) {
